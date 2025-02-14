ee301a6ac9ce971bd0c3b6fde6038c1be944ab03
kamuzki <113306275+kamuzki@users.noreply.github.com>
2025-02-14 20:01:20 +0100

    Initial commit from your app

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 
 function Services() {
   return (
-    <div className="widget">
-      <h2>Unsere Leistungen</h2>
-      <ul className="services-list">
-        <li>Elektroplanung</li>
-        <li>Installationstechnik</li>
-        <li>Automatisierungstechnik</li>
-        <li>Energieberatung</li>
-      </ul>
-    </div>
+    <>
+      <div className="widget">
+        <h2>Elektroplanung</h2>
+        <p>Detaillierte Planung und Konzeptionierung elektrischer Anlagen.</p>
+      </div>
+      <div className="widget">
+        <h2>Installationstechnik</h2>
+        <p>Professionelle Installation und Inbetriebnahme von Elektrotechnik.</p>
+      </div>
+      <div className="widget">
+        <h2>Automatisierungstechnik</h2>
+        <p>Entwicklung und Implementierung von Automatisierungslösungen.</p>
+      </div>
+      <div className="widget">
+        <h2>Energieberatung</h2>
+        <p>Umfassende Beratung zur Optimierung des Energieverbrauchs.</p>
+      </div>
+    </>
   );
 }
 
