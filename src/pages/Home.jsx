6ef644fc555e090ebba5e9973984a226af6ff954
kamuzki <113306275+kamuzki@users.noreply.github.com>
2025-02-14 21:18:29 +0100

    Initial commit from your app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,13 @@ function Home() {
   }, []);
 
   return (
-    <div className={`home-container widget ${isMounted ? 'animate-in' : ''}`}>
-      <h2>Willkommen bei Ihrem Ingenieurbüro für Elektrotechnik</h2>
-      <p className="home-text">
-        Wir sind Ihr kompetenter Partner für innovative Lösungen im Bereich Elektrotechnik. Von der detaillierten Planung bis zur professionellen Umsetzung bieten wir umfassende Dienstleistungen, die auf Ihre individuellen Bedürfnisse zugeschnitten sind.
-      </p>
+    <div className={`home-container`}>
+      <div className={`widget home-widget ${isMounted ? 'animate-in' : ''}`}>
+        <h2>Willkommen bei Ihrem Ingenieurbüro für Elektrotechnik</h2>
+        <p className="home-text">
+          Wir sind Ihr kompetenter Partner für innovative Lösungen im Bereich Elektrotechnik. Von der detaillierten Planung bis zur professionellen Umsetzung bieten wir umfassende Dienstleistungen, die auf Ihre individuellen Bedürfnisse zugeschnitten sind.
+        </p>
+      </div>
     </div>
   );
 }
